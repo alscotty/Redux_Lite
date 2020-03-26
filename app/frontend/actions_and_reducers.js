4b59524eea3560c47726d55b49d7b3dd0f9b8185
alscotty <alawrencescott@gmail.com>
2020-03-25 17:10:52 -0700

    reducers

--- app/frontend/actions_and_reducers.js
+++ app/frontend/actions_and_reducers.js
@@ -4,9 +4,16 @@ const exampleAction = {
     newRole: 'Student'
 }
 
+const requestRoles = {
+    type: 'request roles',
+    roles: ['Student','Teacher', 'Janitor']
+}
+
 const genericReducer = (prevState={}, action) => {
     if (action.type === 'change role'){
         return action.newRole
+    } else if (action.type == 'request roles') {
+        return action.roles
     } else {
         return prevState;
     }
